0cb94b875d829d31023a2a25527987f4bbd054f7
HLN-lobo <98746888+HLN-lobo@users.noreply.github.com>
2022-10-26 11:41:23 -0300

    Tela Inicial

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      <Pressable style={styles.botao}>Avançar</Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		backgroundColor: "#ff4f4f",
+		width: '100%',
+		height: '100%',
+	},
+	botao: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		color: '#FFF',
+    backgroundColor: '#000',
+    padding: '9px',
+    margin: '10px',
+    borderRadius: '10px',
+	},
 });
