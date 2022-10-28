23cee4109ac70f27dd4509e124354e009bfa33dc
HLN-lobo <98746888+HLN-lobo@users.noreply.github.com>
2022-10-28 11:51:09 -0300

    Atualizações Botão

--- App.js
+++ App.js
@@ -1,1 +1,38 @@
-import { StyleSheet, Text, View, Pressable } from 'react-native';js
+import React from 'react';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+
+import AuthProvider from './src/context/MainContext';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from './src/pages/home';
+import Pessoas from './src/pages/Pessoas';
+
+const Stack = createStackNavigator();
+
+export default function App(props){
+    return(
+        <NavigationContainer>
+            <AuthProvider>
+                <Stack.Navigator initialRouteName="home">
+                    <Stack.Screen name="home" component={Home} 
+                    options={{title:'Churrascada', 
+                    headerTitleAlign: 'center', 
+                    headerStyle: { 
+                        height: '35px',
+                        backgroundColor: '#FF4F4F', 
+                        },
+                    }} />
+                    <Stack.Screen name="pessoas" component={Pessoas} 
+                    options={{title: 'Voltar', 
+                    headerTintColor: 'white', 
+                    headerStyle: {
+                        backgroundColor: 'black', 
+                        },
+                    }} />
+                </Stack.Navigator>
+            </AuthProvider>
+        </NavigationContainer>       
+    );
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,1 +1,26 @@
-js
+import React, { useContext } from "react";
+import { StyleSheet , Text, View } from "react-native-web";
+import { TouchableOpacity } from "react-native";
+
+export default function Home(props){
+    return(
+        <View>
+            <TouchableOpacity style={styles.container} title="Avançar" onPress={() => props.navigation.navigate('pessoas')}>
+                <Text style={styles.texto}>Avançar</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        alignCenter: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#000',
+        height: '30px',
+        width: '250px auto',
+    },
+    texto: {
+        color: '#FFF',
+    },
+});
