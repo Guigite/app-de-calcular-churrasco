67b5e01d49ee50d0158957fe9588bdf20f8ac018
Guigite <guicunhaalves@gmail.com>
2022-10-28 11:54:02 -0300

    arrumei

--- src/context/mainContext.js
+++ src/context/mainContext.js
@@ -4,9 +4,8 @@ import { createContext } from "react";
 export const MainContext = createContext();
 
 export default function AuthProvider({children}){
-    
     let valor = 0;
-    
+
     return(
         <MainContext.Provider value={{valor}}>
             {children}
